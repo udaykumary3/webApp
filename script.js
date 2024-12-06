2f2121bbe41066617fb5855b1d3fe673ac08f25e
gs-uyakala <uyakala@gainsight.com>
2024-12-06 14:43:25 +0530

    added js script modified stage

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ window.rudderAnalyticsMount()}window.rudderanalytics.load("2Xx5eab4YxSb0TMDi6UDQ
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement('script');r.async=!0,r.src=a+'?a='+e;
     var c=t.getElementsByTagName('script')[0];c.parentNode.insertBefore(r,c)
-})(window,document,'https://web-sdk.aptrinsic.com/api/aptrinsic.js','AP-FRH2URTTE2GN-2')
+})(window,document,'https://web-sdk.aptrinsic.com/api/aptrinsic.js','AP-FRH2URTTE2GN-2');
 
 
 (function() {
